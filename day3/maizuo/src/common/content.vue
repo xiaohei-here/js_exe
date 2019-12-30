e7f95ded2fc75a78d8eefa5801341ffb80b0f67f
xiaohei-here <17673204806@163.com>
2019-12-30 23:26:22 +0800

    update

--- day3/maizuo/src/common/content.vue
+++ day3/maizuo/src/common/content.vue
@@ -1,7 +1,28 @@
 <template>
-	<div ref="content" class="content">
+	<div class="content">
 		<div>
 			<slot></slot>
 		</div>
 	</div>
 </template>
+
+<script>
+export default {
+	mounted(){
+		new IScroll(this.$refs.content,{
+		
+		})
+	}
+}
+</script>
+
+<style scoped>
+.content {
+	width: 100%;
+	position: absolute;
+	top: 50px;
+	left: 0;
+	bottom: 0;
+	overflow: hidden;
+}
+</style>
