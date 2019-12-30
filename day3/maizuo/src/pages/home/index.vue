b57dba266e8839e0bbbc1c74997eaeb857001313
xiaohei-here <17673204806@163.com>
2019-12-30 23:26:58 +0800

    update

--- day3/maizuo/src/pages/home/index.vue
+++ day3/maizuo/src/pages/home/index.vue
@@ -1,5 +1,14 @@
 <template>
-	<div id="home">
+	<div id="home" class="page">
 		<h1>首页</h1>
 	</div>
 </template>
+
+<script>
+import AppContent from '../../common/content.vue'
+export default {
+	components: {
+		AppContent
+	}
+}
+</script>
