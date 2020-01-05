7cb62c15dbe64dbe87444b5b2b9cbaa6a45dca46
xiaohei-here <17673204806@163.com>
2020-01-05 21:57:50 +0800

    update

--- day3/maizuo/src/App.vue
+++ day3/maizuo/src/App.vue
@@ -1,6 +1,8 @@
 <template>
 <div id="app">
-	<app-header></app-header>
+	<app-header></app-header> 
+	<!-- 路由视图 -->
+	<router-view/>
 	<!--  导航栏  -->
 	<app-nav></app-nav>
 </div>
@@ -9,11 +11,13 @@
 <script>
 import AppHeader from './common/AppHeader.vue'
 import AppNav  from './common/AppNav.vue'
+import content from './common/content.vue'
 
 export default {
 	components: {
 		AppHeader,
-		AppNav
+		AppNav,
+		content
 	}
 }
 </script>
