07b9f1afead72149b9c68b207f97f3d65e26edb6
xiaohei-here <17673204806@163.com>
2020-01-08 23:29:10 +0800

    update

---
+++ day3/maizuo/src/common/Swiper.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="swiper-container" ref="swiper">
+	<div class="swiper-wrapper">
+		<slot></slot>
+	</div>
+	
+	<!-- 如果需要分页器 -->
+	<div class="swiper-pagination"></div>
+</div>
+</template>
+
+<script>
+export default {
+	mothods: {
+		updateDOM(){
+			console.log('调用了更新');
+			swiper.update();
+		}
+	},
+	mounted(){
+		new Swiper(this.$refs.swiper,{
+			pagination: '.swiper-pagination'
+		});
+		console.log("创建");
+	}
+
+}
+</script>
