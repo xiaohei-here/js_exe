eb71d032edb46fcba7fb54ceee39badbc50ea1ce
xiaohei-here <17673204806@163.com>
2020-01-09 23:01:07 +0800

    api

---
+++ day3/maizuo/src/api/index.js
@@ -0,0 +1,21 @@
+// 常量一般使用大写字符调用 
+/*
+首页的轮播请求接口
+参数:
+	_t: 事件戳
+*/
+const HOME_BANNER_URL = '/V4/api/XXXXXXX'
+
+/*  
+正在上映的请求接口 
+参数: 
+	city: 城市名字 
+	page: 页数
+	count: 数量  
+*/
+const NOW_PLAYING_URL = '/v4/api/xxxxx'
+
+export default{
+	HOME_BANNER_URL,
+	NOW_PLAYING_URL	
+}
