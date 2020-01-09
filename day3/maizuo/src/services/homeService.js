5d1a58df5d582914dd2c8ee22b19e2d954e80ded
xiaohei-here <17673204806@163.com>
2020-01-09 23:01:54 +0800

    update

--- day3/maizuo/src/services/homeService.js
+++ day3/maizuo/src/services/homeService.js
@@ -1,13 +1,49 @@
 import axios from 'axios'
+import API from '../api'
 
-function getBannerData(){
-	axios.get('gateway?filmId=4832&k=6573922')
-	.then(response=>{
-		console.log('成功');
-		console.log('失败');
-	})
-	.catch(error=>{
-		console.log('失败');
-		console.log(error);
+export function getHomeBannerData(){
+	// 创建promise请求数据，数据处理成功之后,传递给resolve方法
+	return new Promise((resolve,reject)=>{
+	
+		// 异步请求 
+		axios.get(API.HOME_BANNER_URL,{
+			params: {
+				__t: new Data().getTime()	
+			}
+		})
+		.then(response=>{
+			console.log('成功');
+			console.log(response.data.data.billboards);
+			let data = response.data.data.billboards.map(item=>{
+				return {
+					id: item.id,
+					imgUrl: item.imageUrl
+				}
+			})
+		})
+		.catch(error=>{
+			console.log('失败');
+			console.log(error);
+		})
 	})
 }
+
+export function getNowPlayingData(){
+	return new Promise((resolve,reject)=>{
+		axios.get(API.NOW_PLAYING_url,{
+			__t: new Date().getTime(),
+			page,
+			count
+		})
+		.then(response=>{
+			console.log(response.data.data.films);
+		})
+		.catch(error=>{
+			console.log(error);
+		})
+	})
+	
+}
+
+
+
